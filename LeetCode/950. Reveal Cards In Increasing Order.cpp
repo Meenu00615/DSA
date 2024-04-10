d8b886d7f2034e627cfcf2c74b33e6f62816508e
Meenu Patel <149779716+Meenu00615@users.noreply.github.com>
2024-04-10 23:56:54 +0530

    Add files via upload

---
+++ LeetCode/950. Reveal Cards In Increasing Order.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        sort(deck.rbegin(), deck.rend());
+
+        deque<int> queue;
+
+        for(int card : deck){
+
+            if(!queue.empty()){
+                queue.push_front(queue.back());
+                queue.pop_back();
+            }
+            queue.push_front(card);
+        }
+        return vector<int>(queue.begin(), queue.end());
+    }        
+};
